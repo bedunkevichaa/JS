b47c331e759bdb153816b4b250fd46519231be75
Anton <bedunkevichaa@gmail.com>
2021-05-04 11:06:00 +0300

    add script third task

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 "use strict";
 
-let numberOfFilms;
-
-numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 console.log(numberOfFilms);
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: '',
-    actors: '',
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
 };
 
 console.log(personalMovieDB.count);
+
+// let nameOfFilms = prompt("Один из последних просмотренных фильмов?");
+// let rateOfFilms = prompt("На сколько оцените его?");
+
+const a = prompt("Один из последних просмотренных фильмов?"),
+      b = prompt("На сколько оцените его?"),
+      c = prompt("Один из последних просмотренных фильмов?"),
+      d = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
