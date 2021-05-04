22b91c633755cf75454793887cbc6722c26af2bb
Anton <bedunkevichaa@gmail.com>
2021-05-04 10:32:17 +0300

    add script first task

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
+let numberOfFilms;
 
-// ++incr;
-// --decr;
-
-console.log(incr);
-console.log(decr);
-
-console.log(5%2);
-
-// console.log(2 + 2 * 2 != 8);
-
-const isChecked = true,
-      isClose = false;
-
-console.log(isChecked || !isClose);
+numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+console.log(numberOfFilms);
