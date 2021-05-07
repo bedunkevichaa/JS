513444d1b287e6991b70fb1224465d3b0debe0cc
Anton <bedunkevichaa@gmail.com>
2021-05-07 17:07:36 +0300

    add arrays

--- js/script.js
+++ js/script.js
@@ -1,44 +1,31 @@
 'use strict';
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log('Test');
-    }
-};
+const arr = [1, 12, 3, 89, 8];
+arr.sort();
+console.log(arr);
 
-options.makeTest();
+// // arr[99] = 0;
+// // console.log(arr.length);
 
-const {border, bg} = options.colors;
-console.log(border, bg);
+// arr.forEach(function (item, i, arr) {
+//     console.log(`${i}: ${item} inside array ${arr}`);
+// });
 
-// console.log(Object.keys(options).length);
+//arr.pop();
 
-// //console.log(options['colors']['border']);
 
-// console.log(options.name);
-    
-// // delete options.name;
+// arr.push(10);
+// console.log(arr);
 
-// // console.log(options);
-
-// let counter = 0;
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object') {
-//         for (let i in options[key]) {
-//             console.log(`Property ${i} has value ${options[key][i]}`);
-//             //counter++; 
-//         }
-//     } else {
-//         console.log(`Property ${key} has value ${options[key]}`);
-//         //counter++;
-//     }
-//     counter++;    
+// for ( let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
 // }
-// console.log(counter);
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+const str = prompt('', '');
+const products = str.split(', ');
+products.sort();
+console.log(products.join('; '));
